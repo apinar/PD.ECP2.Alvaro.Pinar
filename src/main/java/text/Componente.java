3b94f863def119c52a180cb8d231e996dfedb1eb
Alvaro <alvaro@apinar>
2014-10-17 23:14:42 +0200

    Creadas clases text.

---
+++ src/main/java/text/Componente.java
@@ -0,0 +1,15 @@
+package text;
+
+public class Componente {
+
+	public void add(Componente h) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object dibujar(boolean b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
