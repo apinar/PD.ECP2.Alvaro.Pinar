e52b818cccc3a11884b678085403e704d052a4bb
Alvaro <alvaro@apinar>
2014-10-19 21:24:14 +0200

    Añadidos el estado abstracto y los concretos.

---
+++ src/main/java/state/connection/Preparado.java
@@ -0,0 +1,41 @@
+package state.connection;
+
+public class Preparado extends State{
+
+	@Override
+	public void abrir() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cerrar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void parar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iniciar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enviar(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recibir(int respuesta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/state/connection/State.java
@@ -0,0 +1,10 @@
+package state.connection;
+
+public abstract class State {
+	public abstract void abrir();
+	public abstract void cerrar();
+	public abstract void parar();
+	public abstract void iniciar();
+	public abstract void enviar(String msg);
+	public abstract void recibir(int respuesta);
+}
